e1a8503dda155581b0eb099da6baf360d1dd0988
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-08-22 00:28:41 +0300

    expores the athentication and use it

--- BackEnd/Routes/Cart.js
+++ BackEnd/Routes/Cart.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { addProductToCart,deletePrductFromCart } = require("../Controllers/Cart")
 
+// MiddileWare
+const authentication= require("../middileware/authentication")
+
 const CartRouter = express.Router();
 
 // req operaters place .
-CartRouter.post("/", addProductToCart);
-CartRouter.delete("/:id",deletePrductFromCart);
+CartRouter.post("/",authentication, addProductToCart);
+CartRouter.delete("/:id",authentication,deletePrductFromCart);
 module.exports = CartRouter;
--- BackEnd/Routes/Products.js
+++ BackEnd/Routes/Products.js
@@ -1,10 +1,14 @@
 const express = require("express");
 // functions place .
 const {createNewProduct,getAllproduct,getProductById}=require("../Controllers/Products");
+
+// MiddileWare
+const authentication= require("../middileware/authentication")
+
 const ProductRouter = express.Router();
 
 // req operaters place .
 ProductRouter.post("/",createNewProduct);
-ProductRouter.get("/",getAllproduct);
+ProductRouter.get("/",authentication,getAllproduct);
 ProductRouter.get("/search_2/:id",getProductById);
 module.exports = ProductRouter;
